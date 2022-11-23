11608adeacefadcc859ea2108f922fe3dfa0c8d6
natanlf <natanlaraferreira@gmail.com>
2022-11-22 22:11:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { message } = require("prompt");
+const prompt = require("prompt");
+
+prompt.start();
+
+let schema = {
+    properties: {
+      primeiroNome: {
+        pattern: /^[a-zA-Z\s\-]+$/,
+        message: 'Name must be only letters, spaces, or dashes',
+        required: true
+      },
+      sobrenome: {
+        pattern: /^[a-zA-Z\s\-]+$/,
+        message: 'Name must be only letters, spaces, or dashes',
+        required: true
+      },
+      email: {
+        pattern: /\w+.+\@\w+\.\w+\.?\w*/g,
+        massage: "Precisa estar no formato de email",
+        require: true
+      },
+      cpf: {
+        pattern: /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/,
+        message: "Precisa ser um cpf válido",
+        require: true
+      }
+    }
+  };
+
+  prompt.get(schema, function (err, result) {
+    //
+    // Log the results.
+    //
+    console.log('Command-line input received:');
+    console.log('  Primeiro Nome: ' + result.primeiroNome);
+    console.log('  Sobrenome: ' + result.sobrenome);
+    console.log('  Email: ' + result.email);
+    console.log('  Cpf: ' + result.cpf);
+  });
